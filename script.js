803b64888820635c905e238851a361801ab35513
BenjiCCB <bensdecker+CCB@gmail.com>
2022-12-10 15:59:01 -0500

    adding local storage

--- script.js
+++ script.js
@@ -1,56 +1,27 @@
+// console.log(dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'));
+
 var saveButtons = $(".saveBtn");
 console.log(saveButtons);
 
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Set colors of hour blocks by time
 $(document).ready(function () {
 
-  console.log("document loaded");
-  console.log(dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'));
-  console.log(dayjs().hour());
-
-  var timeSlots = $(".time-block")
-  console.log(timeSlots[0].attr("data-time"))
-
-
-  
-  // for (var i = 0; i < timeSlots.length; i++) { 
-  //   console.log(timeSlots[i].attr("data-time"))
-  // }
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $('.time-block').each(function(){
+    console.log($(this).attr("data-time"))
+    if ($(this).attr("data-time") < dayjs().hour()){
+      $(this).addClass('past')
+    }
+    else if ($(this).attr("data-time") == dayjs().hour()){
+      $(this).addClass('present')
+    } else{
+      $(this).addClass('future')
+    }
+  })
+ 
 });
 
-// use jquery to grab all timeblack elements -- function updater based on past/present/future
-// loop over timeblocks. check for 1) current time block is less than hour, or present or future
 
-// function saveEntry(event){
-//   event.target
-//   console.log("button clicked", event)
-// }
-
-// saveButtons.on('click', saveEntry);
+// Set save buttons
 saveButtons.on('click', function(){
   var timeValue = $(this).parent().attr("data-time")
   console.log(timeValue)
@@ -62,3 +33,4 @@ saveButtons.on('click', function(){
 });
 
 // create for loop to assign event listener for each button -- can either use object or different storage for each
+// letterBtn.addClass('letter-button btn btn-info'); -- from 05.9
